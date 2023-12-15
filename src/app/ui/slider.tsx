7f76268f0286c57c68e408f15aeeca592cfc6cfb
Diana Cuellar <dcuellar@laprensa.com.sv>
2023-12-15 15:30:04 -0600

    images not reconize

--- src/app/ui/slider.tsx
+++ src/app/ui/slider.tsx
@@ -8,9 +8,9 @@ export default function Slider () {
     return (
         <div className="h-56 sm:h-30 xl:h-80 2xl:h-96">
         <Carousel>
-          <img src="/S1.png" alt="..." />
-          <img src="/S2.png" alt="..." />
-          <img src="/S3.png" alt="..." />
+          <img src="/1.png" alt="..." />
+          <img src="/2.png" alt="..." />
+          <img src="/3.png" alt="..." />
           
         </Carousel>
       </div>
