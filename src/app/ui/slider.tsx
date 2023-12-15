3c50b54494ea76789f346d96c503c67aa7d4e7c5
Diana Cuellar <dcuellar@laprensa.com.sv>
2023-12-15 15:31:30 -0600

    ups

--- src/app/ui/slider.tsx
+++ src/app/ui/slider.tsx
@@ -8,9 +8,9 @@ export default function Slider () {
     return (
         <div className="h-56 sm:h-30 xl:h-80 2xl:h-96">
         <Carousel>
-          <img src="/1.png" alt="..." />
-          <img src="/2.png" alt="..." />
-          <img src="/3.png" alt="..." />
+          <img src="/1.jpg" alt="..." />
+          <img src="/2.jpg" alt="..." />
+          <img src="/3.jpg" alt="..." />
           
         </Carousel>
       </div>
